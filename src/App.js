8bb9d0f87087ef3c01508862fe53e113e0c9dad0
Selma <selma.khaje@gmail.com>
2024-08-26 21:34:25 +0330

    headline created

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import "./index.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1 className="headline">Weather App</h1>
       <WeatherSearch />
       <Footer />
     </div>
